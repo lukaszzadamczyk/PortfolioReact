d718e0b63b1fed05a48aa7a5115c2c0d234178f5
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2021-02-14 20:34:18 +0000

    Add to header text animation

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import BackgroundImage from "../pages/BackgroundImage";
 
 const Header = () => {
+  useEffect(() => {
+    setTimeout(showText, 1000);
+  }, []);
+
+  const showText = () => {
+    const headerWelcome = document.querySelector(".header__welcome");
+    const headerTitle = document.querySelector(".header__title");
+    const headerSpan = document.querySelector(".header__span");
+    headerWelcome.style.opacity = 1;
+    headerTitle.style.opacity = 1;
+    headerSpan.style.opacity = 1;
+  };
   return (
     <>
       <div className="header">
