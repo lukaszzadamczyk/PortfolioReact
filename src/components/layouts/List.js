a82d11799f1e90ee3323ecd967b381714ce3ee7b
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2021-02-21 15:49:01 +0000

    Add a and id to navigation list

--- src/components/layouts/List.js
+++ src/components/layouts/List.js
@@ -2,15 +2,17 @@ import React from "react";
 
 const List = () => {
   const navNames = [
-    { name: "Home", class: "first" },
-    { name: "About Me", class: "second" },
-    { name: "Skills", class: "third" },
-    { name: "Projects", class: "fourth" },
-    { name: "Contact", class: "fifth" },
+    { id: 1, name: "Home", class: "first" },
+    { id: 2, name: "About Me", class: "second" },
+    { id: 3, name: "Skills", class: "third" },
+    { id: 4, name: "Projects", class: "fourth" },
+    { id: 5, name: "Contact", class: "fifth" },
   ];
   const list = navNames.map((item) => (
-    <li key={item.name} className={`menu__item menu__item--${item.class}`}>
-      {item.name}
+    <li key={item.id} className={`menu__item menu__item--${item.class}`}>
+      <a className="menu__link" href={`#${item.class}`}>
+        {item.name}
+      </a>
     </li>
   ));
   return <ul className="menu__list">{list}</ul>;
