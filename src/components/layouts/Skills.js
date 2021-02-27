c17d42a3b6c3bd11cf32eed829154dc33f2a316d
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2021-02-27 19:27:07 +0000

    Created component skills

---
+++ src/components/layouts/Skills.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <section id="third" className="skills">
+      <div className="skills__info">
+        <h2 className="skills__title">Skills</h2>
+        <h3 className="skills__front">Front-End</h3>
+        <p className="skills__text">
+          HTML, CSS, SASS, SCSS, BEM, RWD, ES6, JavaScript, React, ReactHooks,
+          ReactRouter, Redux,
+        </p>
+        <h3 className="skills__front">Others</h3>
+        <p className="skills__text">NPM, GIT, GitHub, VSCode</p>
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
